29a68a3cd0b3cb3430a74dbe5cd9082feac273d2
ChrisChen114 <chen.qianfu114@gmail.com>
2022-12-02 22:12:10 -0800

    feat(project): add new features
    add new features

--- part3/src/index.ts
+++ part3/src/index.ts
@@ -1,5 +1,15 @@
+import {hi} from './m1'
 function sum(a: number, b: number): number {
     return a + b
 }
 
 console.log(sum(123,456))
+console.log('haha');
+console.log('haha')
+
+console.log(hi)
+
+console.log(Promise);
+let fn = (a:number,b:number)=> a+b;
+fn(123,456);
+fn(22,77);
